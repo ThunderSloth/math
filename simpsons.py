8ddafdf1087a32d122d0dff9681bd394cec53d24
Eli Bell <elibell@elibell.net>
2024-04-25 15:10:38 -0400

    Recovered deleted files

---
+++ simpsons.py
@@ -0,0 +1,22 @@
+"""
+... description ...
+
+Eli Bell
+2024-03-05
+"""
+
+
+def main():
+    """ Main method. """
+    x = (54*8)/(12*16)
+    print(9/4)
+    print(x)
+def f(n):
+    return n**3
+
+if __name__ == '__main__':
+    desc = ""
+    print(f"\nWelcome to my {desc} script!\n")
+    main()
+    print(f"\nThanks for using my {desc} script!")
+
